1f1c30e6c04024bfc309284d90fcdceb3de48a79
Katie <katie.herda2@gmail.com>
2021-09-26 02:57:56 +0000

    created an alarm clock / timer which will beep after a specified amount of time has passed based on terminal input.

---
+++ timer1.js
@@ -0,0 +1,23 @@
+//impliment an alarm clock / timer which will beep after a specified amount of time has passed.
+
+const args = process.argv.slice(2);
+
+//If no numbers provided, do not beep
+if (args.length < 1) {
+  return;
+}
+//If negative number or not a number, ignore/skip
+const result = args.filter(num => num > 0);
+
+
+
+const beepTimer = function(result) {
+  for (const el of result) {
+    setTimeout(() => {
+      process.stdout.write('\x07');
+    }, el * 1000);
+  }
+};
+
+beepTimer(result);
+
